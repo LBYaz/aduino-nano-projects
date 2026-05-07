81a4f0cf26d35157ca74e71066df4be4faeb8895
爱吃芒果 <2849674510@qq.com>
2026-05-07 14:55:45 +0800

    update

--- nanostudy/text03/text03.ino
+++ nanostudy/text03/text03.ino
@@ -4,11 +4,11 @@ int p10 = 10;
 void setup() {
     pinMode(2, OUTPUT);//数字2管脚输出，led
     pinMode(3,OUTPUT);
-    pinMode(10,INPUT);//数字3管脚，按钮输入
+    pinMode(10,INPUT);//数字10管脚，振动模块输入
     }
 void loop() {
     boolean value = digitalRead(p10);
-    if(value == LOW){
+    if(value == HIGH){
     digitalWrite(p2, HIGH);
     digitalWrite(p3, HIGH);
     }
