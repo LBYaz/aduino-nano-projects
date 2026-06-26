e7c57f2bd46814a106754b2a5fc19e9de3876e13
爱吃芒果 <2849674510@qq.com>
2026-06-26 14:51:27 +0800

    update

--- nanostudy/text08/text08.ino
+++ nanostudy/text08/text08.ino
@@ -1,13 +1,13 @@
 int AO = A0;
 int DO = 2;
 void setup(){
-  Serial.begin(9600);
+  Serial.begin(9600);默认
   pinMode(DO,INPUT);
 }
 void loop(){
   int adc = analogRead(AO);
   int doState = digitalRead(DO);
-  Serial.print("ADC原始值：");印
+  Serial.print("ADC原始值：");
   Serial.print(adc);
   Serial.print(" DO电平：");
   Serial.println(doState);
